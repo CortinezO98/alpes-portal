4ba2dadc8ee38fef0be8b84e1dea3ac1c9759cf4
CortinezO98 <153117790+CortinezO98@users.noreply.github.com>
2026-08-27 21:59:34 -0500

    feat(platform): configure custom user postgres and docker

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,93 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ("email",)
+
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "role",
+        "is_staff",
+        "is_active",
+        "is_email_verified",
+    )
+
+    list_filter = (
+        "role",
+        "is_staff",
+        "is_active",
+        "is_email_verified",
+    )
+
+    search_fields = (
+        "email",
+        "first_name",
+        "last_name",
+    )
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "email",
+                    "password",
+                )
+            },
+        ),
+        (
+            "Información personal",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                )
+            },
+        ),
+        (
+            "Acceso",
+            {
+                "fields": (
+                    "role",
+                    "is_email_verified",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (
+            "Fechas importantes",
+            {
+                "fields": (
+                    "last_login",
+                    "date_joined",
+                )
+            },
+        ),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "role",
+                    "is_staff",
+                    "is_active",
+                ),
+            },
+        ),
+    )
