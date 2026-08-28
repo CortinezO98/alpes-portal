a869a0f2a06e2915c706a55059c04c9784200ebb
CortinezO98 <153117790+CortinezO98@users.noreply.github.com>
2026-08-28 10:46:37 -0500

    feat(auth): force safe USER role for public and social signups

---
+++ apps/accounts/adapters.py
@@ -0,0 +1,33 @@
+from allauth.account.adapter import DefaultAccountAdapter
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+
+from .models import User
+
+
+class AlpesAccountAdapter(DefaultAccountAdapter):
+    def save_user(self, request, user, form, commit=True):
+        user = super().save_user(request, user, form, commit=False)
+        user.role = User.Role.USER
+        user.is_staff = False
+        user.is_superuser = False
+        if commit:
+            user.save()
+        return user
+
+    def confirm_email(self, request, email_address):
+        email_address = super().confirm_email(request, email_address)
+        user = email_address.user
+        if not user.is_email_verified:
+            user.is_email_verified = True
+            user.save(update_fields=("is_email_verified",))
+        return email_address
+
+
+class AlpesSocialAccountAdapter(DefaultSocialAccountAdapter):
+    def save_user(self, request, sociallogin, form=None):
+        user = sociallogin.user
+        user.role = User.Role.USER
+        user.is_staff = False
+        user.is_superuser = False
+        user = super().save_user(request, sociallogin, form=form)
+        return user
